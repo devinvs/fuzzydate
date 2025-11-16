75770a7ed3c4f3d8a7ebf37c816b4508678623e8
Connor Keane <connor.keane@kxnr.me>
2025-11-16 00:39:57 -0800

    update cli to always use aware logic

--- fuzzydate-cli/src/main.rs
+++ fuzzydate-cli/src/main.rs
@@ -1,16 +1,15 @@
-use std::{fmt::Display, process::exit};
+use std::{fmt::Display, process::exit, str::FromStr};
 
+use chrono::Local;
+use chrono_tz::Tz;
 use clap::Parser;
-use fuzzydate::{parse, parse_relative_to, parse_with_default_time};
+use fuzzydate::{aware_parse, parse, parse_relative_to, parse_with_default_time};
 
 #[derive(Parser, Debug)]
 struct Args {
     // TODO: handle timezones
-    #[arg(short, long, default_value = "%Y-%m-%dT%H:%M:%S")]
+    #[arg(short, long, default_value = "%Y-%m-%dT%H:%M:%S%Z")]
     format: String,
-
-    #[arg(short, long, group = "base")]
-    default_time: Option<String>,
 
     #[arg(short, long, group = "base")]
     relative_to: Option<String>,
@@ -40,28 +39,23 @@ fn unwrap_or_report<T, E: Display>(arg: Result<T, E>) -> T {
 fn main() {
     let args = Args::parse();
 
-    let result = match args {
-        Args {
-            default_time: Some(default_time),
-            date_string,
-            ..
-        } => {
-            let default_time = unwrap_or_report(parse(default_time));
-            unwrap_or_report(parse_with_default_time(
-                date_string.join(" "),
-                default_time.time(),
-            ))
-        }
-        Args {
-            relative_to: Some(relative_to),
-            date_string,
-            ..
-        } => {
-            let relative_to = unwrap_or_report(parse(relative_to));
-            unwrap_or_report(parse_relative_to(date_string.join(" "), relative_to))
-        }
-        Args { date_string, .. } => unwrap_or_report(parse(date_string.join(" "))),
-    };
+    let fuzzy_str = args.date_string.join(" ");
+
+    // TODO: fix the expects here
+    // TODO: fix the timezones
+    let input_tz = args
+        .input_timezone
+        .map_or(Local, |tz| Tz::from_str(&tz).expect("Invalid Timezone"));
+    let output_tz = args
+        .output_timezone
+        .map_or(Local, |tz| Tz::from_str(&tz).expect("Invalid timezone"));
+
+    let relative_to = args
+        .relative_to
+        .map(|dt| aware_parse(dt, None, input_tz).expect("Invalid datetime"));
+
+    let result =
+        aware_parse(args.date_string.join(" "), relative_to, input_tz).expect("Parse Error");
 
     println!("{}", result.format(&args.format));
 }
